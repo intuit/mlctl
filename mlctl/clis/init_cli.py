ae32e1aa3c147f8d325eee48dafb27a6371713f8
deepk2u <deepk2u@gmail.com>
2021-07-09 00:26:40 -0700

    fixup: includes generic path to template folder

--- mlctl/clis/init_cli.py
+++ mlctl/clis/init_cli.py
@@ -1,8 +1,11 @@
 import click
+import os
 from cookiecutter.main import cookiecutter
 
 
 @click.command(name='init', help="init command")
 @click.option('--template', '-t', help="Location of the project template github location.", metavar='')
 def init(template):
-    cookiecutter(template if template else 'template/')
+    template_path = template if template else os.path.join(
+        os.path.dirname(__file__), 'template')
+    cookiecutter(template_path)
