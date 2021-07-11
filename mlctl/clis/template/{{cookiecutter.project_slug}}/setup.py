c646d8a2bf253f89a1d65db28ae613c9ac40d0d8
deepk2u <deepk2u@gmail.com>
2021-07-11 11:05:30 -0700

    fixup: includes generic path to template folder (#13)
    * fixup: includes generic path to template folder
    
    * fixup: code refactoring
    
    * fixup: dependencies pointing to versions

--- mlctl/clis/template/{{cookiecutter.project_slug}}/setup.py
+++ mlctl/clis/template/{{cookiecutter.project_slug}}/setup.py
@@ -5,13 +5,8 @@ setup(
     version='{{ cookiecutter.version }}',
     description="{{ cookiecutter.project_short_description }}",
     packages=find_packages(),
-    install_requires=[
-        'tensorflow==1.13.1',
-        'pandas==0.24.2',
-        'featurizer>=0.0.29',
-    ],
+    install_requires=[],
     python_requires='>=3.6',
-    #dockerlines="ARG DOCKER_IMAGE_NAME=${paws.registry}/${paws.gitOrg}/${paws.modal_name}/service/${paws.modal_name}:${DOCKER_TAGS}",,
     entry_points={
         'baklava.train': [
             'example_train = model.train:train'
