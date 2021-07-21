495b072ca4e2227190e2a8be04a3dafe9b64d1f5
Alex Chung <alex@socialg.tech>
2021-07-21 21:32:36 +0000

    Add mlctl integration with Baklava for building a training container Put placeholder for container push to a repo

--- mlctl/clis/train_cli.py
+++ mlctl/clis/train_cli.py
@@ -1,11 +1,55 @@
 import click
 from mlctl.utils import determine_plugin
-
+import os
+import subprocess
 
 @click.group(name='train', help="Train commands")
 def train():
     pass
 
+import sys
+import time
+@train.command(name="build", help="build a container for training")
+def start():
+    click.echo("Building container for training job")
+
+    # TODO: make this a dynamic lookup
+    if not os.path.isfile('./setup.py'):
+        print('Missing mlctl setup.py for building a mlctl universal container. \
+        Try mlctl init, or navigating to the home directory of the project.')
+        return
+    
+    process = subprocess.Popen(['python3', './setup.py', 'train', '-t', 'train-image'],stdout=subprocess.PIPE)
+    while True:
+        output = process.stdout.readline()
+        if process.poll() is not None:
+            break
+        if output:
+            print (output.strip().decode())
+    retval = process.poll()
+    return
+
+@train.command(name="push", help="push a container for training")
+def start():
+
+    # TODO:
+    # Remove placeholder
+    # check a state file for the container repo
+    # tag the local image to the remote
+    # push to the right repo
+    click.echo("Pushing container for training job")
+    def spinning_cursor():
+        while True:
+            for cursor in '|/-\\':
+                yield cursor
+
+    spinner = spinning_cursor()
+    for _ in range(50):
+        sys.stdout.write(next(spinner))
+        sys.stdout.flush()
+        time.sleep(0.1)
+        sys.stdout.write('\b')
+    
 
 @train.command(name="start", help="Train a model")
 @click.option('--profile', '-pr', envvar='PROFILE', help="credentials profile or file location", metavar='')
